731fb23f09d8f27fb42e23fc3e6685280679393d
ewelinaMakowska <ewelina.makowska0@gmail.com>
2019-08-15 21:16:01 +0200

    basic project setup

---
+++ src/components/Quote.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <slot> </slot>
+    </div>
+</template>
+
+<script>
+export default {
+ 
+}
+</script>
+
+<style scoped>
+div {
+    font-family: 'Roboto', sans-serif;
+    border: 4px solid blueviolet;
+    padding: 30px;
+    margin: 20px;
+    color: #555;
+    font-size: 20px;
+    text-align: center;
+}
+</style>
